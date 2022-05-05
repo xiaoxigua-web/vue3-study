2b646404e93598097da143cc872ca7c96ce9827f
xiaozhicheng <657286937@qq.com>
2022-05-05 14:01:59 +0800

    first commit

---
+++ src/pages/home.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>这是首页</h1>
+  <button @click="toggle">click</button>
+  <transition name="fade">
+    <h1 v-if="showTitle">你好 Vue 3</h1>
+  </transition>
+  <TodoList />
+</template>
+
+<script setup>
+import TodoList from '../components/TodoList.vue'
+import { ref } from 'vue'
+let showTitle = ref(true)
+function toggle() {
+  showTitle.value = !showTitle.value
+}
+</script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s linear;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
